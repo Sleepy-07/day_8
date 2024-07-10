543978425757f0774734f1f3cfb23edeb779e071
Sleepy-07 <sahilvrmji@gmail.com>
2024-07-10 16:44:51 +0530

    Activity@2
    2nd layout of Linear layout

---
+++ app/src/main/java/com/example/day_8/LinearActivity2.kt
@@ -0,0 +1,20 @@
+package com.example.day_8
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LinearActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_linear2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
