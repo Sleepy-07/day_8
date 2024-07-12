1e73b5da800b2cae5c7f80d13b135e20adf69bf8
Sleepy-07 <sahilvrmji@gmail.com>
2024-07-12 16:08:16 +0530

    application_lifecycle

--- app/src/main/java/com/example/day_8/MainActivity.kt
+++ app/src/main/java/com/example/day_8/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.day_8
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var btn : Button? = null
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,47 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btn = findViewById(R.id.btnsaveclick)
+        btn?.setOnClickListener{
+            Toast.makeText(this,"Item is saved " , Toast.LENGTH_SHORT).show()
+        }
+
+        btn = findViewById(R.id.btncancleclick)
+        btn?.setOnClickListener{
+            Toast.makeText(this,"item is cancled " , Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"On start", Toast.LENGTH_SHORT).show()
+
+    }
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"On resume", Toast.LENGTH_SHORT).show()
+
+    }
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this,"On Stop", Toast.LENGTH_SHORT).show()
+
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"On Restart", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"On Destroy", Toast.LENGTH_SHORT).show()
+
+    }
+
+
 }
--- app/src/main/java/com/example/day_8/maan2.kt
+++ app/src/main/java/com/example/day_8/maan2.kt
@@ -1,45 +1,30 @@
 package com.example.day_8
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LinearActivity2 : AppCompatActivity() {
+class Maan2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Toast.makeText(this,"On create",Toast.LENGTH_SHORT).show()
+//        var btn:Button? = null
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_linear2)
+        setContentView(R.layout.activity_maan2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        btn = findViewById(R.id.math)
+//        btn?.setOnClickListener{
+//         Toast.makeText(this,"on start",Toast.LENGTH_SHORT).show()
+//
+//        }
 
-
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Toast.makeText(this,"On start",Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Toast.makeText(this,"On resume", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Toast.makeText(this,"On stop", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Toast.makeText(this,"On destroy", Toast.LENGTH_SHORT).show()
     }
 }
